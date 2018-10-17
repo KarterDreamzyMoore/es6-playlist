1b63d9620d9b81e17105f72bc14e7deb92b7cc65
KarterDreamzyMoore <kmoore3365@myhta.org>
2018-10-17 10:46:54 -1000

    Done

---
+++ app.js
@@ -0,0 +1,38 @@
+window.onload = function() {
+    const form = document.querySelector('#form');
+    const genre = document.querySelector('#genre');
+    const container = document.querySelector("#container");
+    
+    console.log(data);
+
+
+    let dataDiv = document.querySelector('#data');
+    form.addEventListener('submit', function(event){ 
+    event.preventDefault();
+    container.innerHTML = data.filter((filter) => {
+
+        console.log('genre', genre.value);
+        return filter.genre === genre.value;
+
+    })
+
+
+    .map((val) => {
+
+        return `
+        <div id = "songBox">
+            <div id = "title">
+                <h2>${val.song}</h2>
+            </div>
+            <img src=${val.image} width="250" height="auto">
+            <h3>${val.artist}</h3>
+            <br>
+            <br>
+        </div>
+        `
+
+    })
+    .join("");
+    });
+    
+}
---
+++ data.js
@@ -0,0 +1,79 @@
+const data = [
+    {
+        artist:'Lil Uzi Vert',
+        genre:'Hip hop',
+        song:'New Patek',
+        image:"https://sslf.ulximg.com/image/750x750/cover/1537245590_c93c4b943e9872b8eecf67cf004ccaa0.jpg/5121ff4f4d1f0b2f7e765d5b47efbbd4/1537245590_e1514e5bbf02d71203480032e3c8425c.jpg",
+    },
+    {
+        artist:'Playboi Carti',
+        genre:'Hip hop',
+        song:'Shoota',
+        image:"https://images.genius.com/3d5481eedcae5b24a6de3f60128d5bf8.1000x1000x1.jpg",
+        
+    },
+    {
+        artist:'Drake',
+        genre:'Hip hop',
+        song:'Nonstop',
+        image:'https://www.billboard.com/files/styles/article_main_image/public/media/drake-nonstop-billboard-1548.jpg',
+        
+    },
+    {
+        artist:'Migos',
+        genre:'Hip hop',
+        song:'Ric Flair Drip',
+        image:"https://images.genius.com/5beeddd87ab560c800bf30da49aa973e.500x500x1.jpg",
+    },
+    {
+        artist:'Kodak Black',
+        genre:'Hip hop',
+        song:'ZEZE',
+        image:"https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fimage%2F2018%2F10%2Fkodak-black-travis-scott-offset-zeze-stream-01.jpg?q=75&w=800&cbr=1&fit=max",
+    },
+    {
+        artist:'YG',
+        genre:'Hip hop',
+        song:'Big Bank',
+        image:"https://i.ytimg.com/vi/z61GFxVD8K4/maxresdefault.jpg",
+        
+    },
+    {
+        artist:'Lil Peep',
+        genre:'The Peep',
+        song:'Falling Down',
+        image:"https://i.ytimg.com/vi/-jRKsiAOAA8/maxresdefault.jpg",
+        
+    },
+    {
+        artist:'Lil Peep',
+        genre:'The Peep',
+        song:'Sunlight on your skin',
+        image:"https://i.ytimg.com/vi/0cOkAlyWBXk/maxresdefault.jpg",
+    },
+    {
+        artist:'Lil Peep',
+        genre:'The Peep',
+        song:'Benz Truck',
+        image:"https://medias.spotern.com/spots/w640/110020-1532336916.jpg",
+    },
+    {
+        artist:'Lil Peep',
+        genre:'The Peep',
+        song:'4 Gold Chains',
+        image:"https://i.ytimg.com/vi/zt83PJFJKFU/maxresdefault.jpg",
+    },
+    {
+        artist:'Lil Peep',
+        genre:'The Peep',
+        song:'Save that ****',
+        image:"https://media.altpress.com/uploads/2018/07/lil_peep_save_that_shit.jpg",
+    },
+    {
+        artist:'Lil Peep',
+        genre:'The Peep',
+        song:'Spotlight',
+        image:"https://i.ytimg.com/vi/b7K_lbglfI4/maxresdefault.jpg",
+    }];
+
+
